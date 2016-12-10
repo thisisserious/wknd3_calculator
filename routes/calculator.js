e3521be81ab256d5e9d3a54089bb157c31328def
Laura Abend <laura.l.abend@gmail.com>
2016-12-10 16:18:57 -0600

    include subtraction, multiplication, division, and switch statement for calculations

--- routes/calculator.js
+++ routes/calculator.js
@@ -16,12 +16,32 @@ console.log('nums:', nums);
 //   });
 
 router.post('/', function (req, res) {
-    console.log('req.body:', req.body);
+    console.log('calculator.js req.body:', req.body);
+    var mathOperation = req.body.mathOperation;
     var add = parseInt(req.body.firstNum) + parseInt(req.body.secondNum);
     var sum = add.toString();
-    console.log('add:', add);
-    console.log('sum:', sum);
-    res.send(sum);
+    var subtract = parseInt(req.body.firstNum) - parseInt(req.body.secondNum);
+    var diff = subtract.toString();
+    var multiply = parseInt(req.body.firstNum) * parseInt(req.body.secondNum);
+    var product = multiply.toString();
+    var divide = parseInt(req.body.firstNum) / parseInt(req.body.secondNum);
+    var quotient = divide.toString();
+    switch (mathOperation) {
+      case 'add':
+        res.send(sum);
+        break;
+      case 'subtract':
+        res.send(diff);
+        break;
+      case 'multiply':
+        res.send(product);
+        break;
+      case 'divide':
+        res.send(quotient);
+        break;
+      default:
+        console.log('Boo.');
+    }
   });
 
 module.exports = router;
